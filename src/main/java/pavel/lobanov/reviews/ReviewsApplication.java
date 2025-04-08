be1b5897f864e491819e9cd1a8641ae78eba1846
Pavel Lobanov <pavel.lobanov@uni-konstanz.de>
2025-04-08 10:49:26 +0200

    init

---
+++ src/main/java/pavel/lobanov/reviews/ReviewsApplication.java
@@ -0,0 +1,13 @@
+package pavel.lobanov.reviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsApplication.class, args);
+	}
+
+}
